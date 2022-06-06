d95e800ceca3249e01c952bde93e210439849b19
Noble <nojess143@gmail.com>
2022-06-06 06:05:15 -0700

    Multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def multiple_return(sentence):
-    my_turple = ()
+def multiple_returns(sentence):
+    my_tuple = ()
     if len(sentence) == 0:
-        my_turple = 0, "None"
+        my_tuple = 0, "None"
     else:
-        my_turple = len(sentence), sentence[0]
-    return my_turple
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
