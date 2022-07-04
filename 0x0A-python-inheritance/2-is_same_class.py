2e7df4a420b62a5fb615884058ae3af199606d2a
Noble <nojess143@gmail.com>
2022-07-04 11:00:18 -0700

    Use the isinstance built-in function

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Function that defines the instance of a class."""
+
+
+def is_same_class(obj, a_class):
+    """Checks the instance of an object to a class.
+    or if they are both same.
+
+    Args:
+        -obj: object to check the instance
+        -a_class: Class to check it to.
+        """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
