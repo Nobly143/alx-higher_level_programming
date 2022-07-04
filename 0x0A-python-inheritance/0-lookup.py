4337535ea51ff68e2a9a7b774564c93447cbb8ce
Noble <nojess143@gmail.com>
2022-07-04 10:26:07 -0700

    Look into an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,9 +3,6 @@
     in an object."""
 
 
-    def lookup(obj):
-        """Returns list of attributes in an object.
-
-        Args: Takes obj as the object."""
-
-        return dir(obj)
+def lookup(obj):
+    """Returns list of attributes in an object."""
+    return dir(obj)
