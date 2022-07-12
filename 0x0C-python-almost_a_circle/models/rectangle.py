b66a3c599333573ea00b4a991584eefaabda76d0
Noble <nojess143@gmail.com>
2022-07-12 10:33:34 -0700

    Model update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
-"""A model of a rectangle class"""
-
+"""Module rectangle.
+Create a Rectangle class, inheriting from Base.
+"""
+import json
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Initialise the parts of the rectangle"""
+    """Class describing a rectangle.
+    Public instance methods:
+        - area()
+        - display()
+        - to_dictionary()
+        - update()
+    Inherits from Base.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize each part of thr rectangle"""
+        """Initializes a Rectangle instance.
+        Args:
+            - __width: width
+            - __height: height
+            - __x: position
+            - __y: position
+            - id: id
+        """
 
         self.width = width
         self.height = height
@@ -16,58 +32,92 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-        @property
-        def width(self):
-            """Retrieves the width attribute."""
-            return self.__width
+    @property
+    def width(self):
+        """Retrieves the width attribute."""
 
-        @width.setter
-        def width(self, value):
-            """Set the width value"""
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = value
+        return self.__width
 
-        @property
-        def height(self):
-            """Retrieve the height attribute"""
-            return self.__height
+    @property
+    def height(self):
+        """Retrieves the height attribute."""
 
-        @height.setter
-        def height(self, value):
-            """Set the height value"""
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
+        return self.__height
 
-        @property
-        def x(self):
-            """Retrieve the x attribute"""
-            return self.__x
+    @property
+    def x(self):
+        """Retrieves the x attribute."""
 
-        @x.setter
-        def x(self, value):
-            """set the x value"""
-            if type(value) is not int:
-                raise TypeError("x must be an integer")
-            if value <= 0:
-                raise ValueError("x must be > 0")
-            self.__x = value
+        return self.__x
 
-        @property
-        def y(self):
-            """Retrieve the y attribute"""
-            return self.__y
+    @property
+    def y(self):
+        """Retrieves the y attribute."""
 
-        @y.setter
-        def y(self, value):
-            """set the y value"""
-            if type(value) is not int:
-                raise TypeError("y must be an integer")
-            if value <= 0:
-                raise ValueError("y must be > 0")
-            self.__y = value
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """Sets the width attribute."""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Sets the height attribute."""
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """Sets the x attribute."""
+
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """Sets the y attribute."""
+
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Calculates the area of a Rectangle instance.
+        Returns: area
+        """
+
+        return self.__width * self.__height
+
+    def display(self):
+        """Prints the rectangle with # to the stdout."""
+
+        for y in range(0, self.__y):
+            print("")
+        for i in range(0, self.__height):
+            for x in range(0, self.__x):
+                print(" ", end="")
+            for j in range(0, self.__width):
+                print("#", end="")
+            print("")
+
+    def __str__(self):
+        """Return a string representation of the class"""
+
+        s = "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.__x, self.__y, self.__width, self.__height)
+        return s
