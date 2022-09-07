6774a3b37eb28ca11863b2f1d8dbc3848c3d9ac1
Noble <nojess143@gmail.com>
2022-09-07 15:13:36 -0700

    update sub class

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,18 +6,22 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
   }
+
   rotate () {
     const h = this.height;
     const w = this.width;
+    this.width = h;
     this.height = w;
-    this.width =h;
   }
+
   double () {
     this.height *= 2;
     this.width *= 2;
   }
 }
+
 module.exports = Rectangle;
