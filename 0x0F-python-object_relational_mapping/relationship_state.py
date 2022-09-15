33eee7cf1992823debb1f9b711ddf6b67f330ddd
Noble <nojess143@gmail.com>
2022-09-15 15:58:07 -0700

    SQL Alchemy

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Definition of the State class"""
+"""Definition of the State class with relationship to City"""
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
-Base = declarative_base()
+from sqlalchemy.orm import relationship
+from relationship_city import City, Base
 
 
 class State(Base):
@@ -14,3 +14,5 @@ class State(Base):
     id = Column(Integer, autoincrement=True,
                 primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
